ae0271bb21e1f90014219366402fda88bdb58ccf
chrysStam <creating4art@gmail.com>
2021-12-19 23:47:28 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Code.Learn - Project Future 7: Team 6
+
+**Team:**
+`Chrysoula Stamatiou`
+`Georgios Gagrinas`
+`Sofronios Sartinas`
+`Theodoros Mystiloglou`
+
---
+++ smdb-spring-project-api/src/main/java/gr/codelearn/smdb/api/SmdbApplication.java
@@ -0,0 +1,20 @@
+package gr.codelearn.smdb.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class SmdbApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(SmdbApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmdbApplication.class, args);
+
+        logger.info("Starting program execution");
+
+    }
+}
