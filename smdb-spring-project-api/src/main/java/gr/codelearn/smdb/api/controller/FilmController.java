607cf909303fba687f8d765cec92b7f6cb191ebb
sofro4 <sofron4@hotmail.gr>
2022-01-06 15:35:06 +0200

    Add in films and tv_shows: findByTitle searchByTitle findTopRatings -> num as a parameter and return the top num movies/tvshows with the highest imdb_score findByContributor -> name, surname as parameters return the movies/tvshows associated with the individual findByContributorAndRole -> name,surname,role as parameters return the movies/tvshow associated with  individual with that role findAllByGenresIn -> return movies/tvshows for a given genre

--- smdb-spring-project-api/src/main/java/gr/codelearn/smdb/api/controller/FilmController.java
+++ smdb-spring-project-api/src/main/java/gr/codelearn/smdb/api/controller/FilmController.java
@@ -2,6 +2,8 @@ package gr.codelearn.smdb.api.controller;
 
 import gr.codelearn.smdb.api.domain.Film;
 import gr.codelearn.smdb.api.domain.Genre;
+import gr.codelearn.smdb.api.domain.Person;
+import gr.codelearn.smdb.api.domain.Role;
 import gr.codelearn.smdb.api.service.BaseService;
 import gr.codelearn.smdb.api.service.FilmService;
 import gr.codelearn.smdb.api.transfer.ApiResponse;
@@ -31,11 +33,11 @@ public class FilmController extends AbstractController<Film> {
 		return filmService.findByTitle(title);
 	}
 
-	@GetMapping(params = {"genre"})
-	public ResponseEntity<ApiResponse<List<Film>>> find2(@RequestParam("genre") Set<Genre> genre) {
+	@GetMapping(path= "search", params = {"title"})
+	public ResponseEntity<ApiResponse<List<Film>>> searchByTitle(@RequestParam("title") String title) {
 		return ResponseEntity.ok(ApiResponse.<List<Film>>builder()
-										 .data(filmService.findAllByGenresIn(genre))
-										 .build());
+											.data(filmService.searchByTitle(title))
+											.build());
 	}
 
 
@@ -47,10 +49,31 @@ public class FilmController extends AbstractController<Film> {
 	}
 
 
-	@GetMapping(path= "search", params = {"title"})
-	public ResponseEntity<ApiResponse<List<Film>>> searchByTitle(@RequestParam("title") String title) {
+
+
+	@GetMapping(params = {"name","surname"})
+	public ResponseEntity<ApiResponse<List<Film>>> findByContributor(@RequestParam("name") String name,
+																	   @RequestParam("surname") String surname) {
 		return ResponseEntity.ok(ApiResponse.<List<Film>>builder()
-											.data(filmService.searchByTitle(title))
+											.data(filmService.findByContributor(name,surname))
 											.build());
 	}
+
+	@GetMapping(params = {"name","surname","role"})
+	public ResponseEntity<ApiResponse<List<Film>>> findByContributorAndRole(@RequestParam("name") String name,
+																	 @RequestParam("surname") String surname,
+																			@RequestParam("role") Role role) {
+		return ResponseEntity.ok(ApiResponse.<List<Film>>builder()
+											.data(filmService.findByContributorAndRole(name,surname,role))
+											.build());
+	}
+
+
+	@GetMapping(params = {"genre"})
+	public ResponseEntity<ApiResponse<List<Film>>> find2(@RequestParam("genre") Set<Genre> genre) {
+		return ResponseEntity.ok(ApiResponse.<List<Film>>builder()
+											.data(filmService.findAllByGenresIn(genre))
+											.build());
+	}
+
 }
--- smdb-spring-project-api/src/main/java/gr/codelearn/smdb/api/repository/FilmRepository.java
+++ smdb-spring-project-api/src/main/java/gr/codelearn/smdb/api/repository/FilmRepository.java
@@ -2,6 +2,9 @@ package gr.codelearn.smdb.api.repository;
 
 import gr.codelearn.smdb.api.domain.Film;
 import gr.codelearn.smdb.api.domain.Genre;
+import gr.codelearn.smdb.api.domain.Person;
+import gr.codelearn.smdb.api.domain.Role;
+import gr.codelearn.smdb.api.domain.TVShow;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,11 +17,25 @@ import java.util.Set;
 public interface FilmRepository extends JpaRepository<Film, Long> {
 
 	Film findByTitle(String title);
-	List<Film> findAllByGenresIn(Set<Genre> genre);
+	@Query("select DISTINCT f from Film f WHERE lower(f.title) LIKE %:title% ")
+	List<Film> searchByTitle(String title);
+
+
 
 	@Query("select DISTINCT f from Film f ORDER BY f.imdbScore DESC ")
 	List<Film> findTopRating(PageRequest pageable);
 
-	@Query("select DISTINCT f from Film f WHERE lower(f.title) LIKE %:title% ")
-	List<Film> searchByTitle(String title);
+
+
+	@Query("select DISTINCT f from Film f JOIN f.contentContributors c JOIN c.person p where p.id =(select DISTINCT p" +
+			".id from Person p WHERE p.name = :name and p.surname=:surname)")
+	List<Film> findByContributor(String name,String surname);
+
+	@Query("select DISTINCT f from Film f JOIN f.contentContributors c JOIN c.key k JOIN c.person p where p.id =" +
+			"(select DISTINCT p.id from Person p WHERE p.name = :name and p.surname=:surname) AND (k.role=:role) ")
+	List<Film> findByContributorAndRole(String name, String surname, Role role);
+
+	List<Film> findAllByGenresIn(Set<Genre> genre);
+
+
 }
