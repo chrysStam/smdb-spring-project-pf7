1a67e83580f9ba5f5cf4f6a0ad8f65cdd7166ff6
sofro4 <sofron4@hotmail.gr>
2022-01-08 15:50:36 +0200

    Creater ReportController

---
+++ smdb-spring-project-api/src/main/java/gr/codelearn/smdb/api/repository/ReportRepository.java
@@ -0,0 +1,44 @@
+package gr.codelearn.smdb.api.repository;
+
+import gr.codelearn.smdb.api.domain.Content;
+import gr.codelearn.smdb.api.domain.Film;
+import gr.codelearn.smdb.api.domain.Genre;
+import gr.codelearn.smdb.api.domain.Person;
+import gr.codelearn.smdb.api.domain.Role;
+import gr.codelearn.smdb.api.domain.TVShow;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Set;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Content, Long> {
+
+	Film findByTitle(String title);
+	@Query("select DISTINCT f from Content f WHERE lower(f.title) LIKE %:title% ")
+	List<Content> searchByTitle(String title);
+
+
+
+	@Query("select DISTINCT f from Content f ORDER BY f.imdbScore DESC ")
+	List<Content> findTopRating(PageRequest pageable);
+
+
+
+	@Query("select DISTINCT f from Content f JOIN f.contentContributors c JOIN c.person p where p.id =(select " +
+			"DISTINCT p" +
+			".id from Person p WHERE p.name = :name and p.surname=:surname)")
+	List<Content> findByContributor(String name,String surname);
+
+	@Query("select DISTINCT f from Content f JOIN f.contentContributors c JOIN c.key k JOIN c.person p where p.id =" +
+			"(select DISTINCT p.id from Person p WHERE p.name = :name and p.surname=:surname) AND (k.role=:role) ")
+	List<Content> findByContributorAndRole(String name, String surname, Role role);
+
+	List<Content> findAllByGenresIn(Set<Genre> genre);
+
+	@Query("SELECT count(c) FROM Content c")
+	Integer findNative();
+}
