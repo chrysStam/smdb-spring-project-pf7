2694444994f57de58a19a19121dff46dc7d02137
gagrinasg <gagrinas.g@gmail.com>
2022-01-09 16:45:11 +0200

    Completed person layers

--- smdb-spring-project-api/src/main/java/gr/codelearn/smdb/api/repository/PersonRepository.java
+++ smdb-spring-project-api/src/main/java/gr/codelearn/smdb/api/repository/PersonRepository.java
@@ -1,8 +1,10 @@
 package gr.codelearn.smdb.api.repository;
 
 import gr.codelearn.smdb.api.domain.Content;
+import gr.codelearn.smdb.api.domain.Film;
 import gr.codelearn.smdb.api.domain.Person;
 import gr.codelearn.smdb.api.domain.Role;
+import gr.codelearn.smdb.api.domain.TVShow;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,4 +16,13 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
 
 	@Query("select c from Content c join c.contentContributors cC where cC.key.personId = ?1 and cC.key.role = ?2")
 	List<Content> getContributionsOfPersonByIdByRole(Long personId, Role role);
+
+	@Query("select tv from TVShow tv join tv.contentContributors tvC where tvC.key.personId = ?1")
+	List<TVShow> getTVShowContributionsOfPersonById(Long personId);
+
+	@Query("select f from Film f join f.contentContributors fC where fC.key.personId = ?1")
+	List<Film>getFilmContributionsOfPersonById(Long personId);
+
+	@Query("select c from Content c join c.contentContributors cC where cC.key.personId = ?1")
+	List<Content>getContributionsOfPersonById(Long personId);
 }
