bbf55e17b8c3265937467b077ec8d56607d49720
chrysStam <creating4art@gmail.com>
2022-01-10 04:28:50 +0200

    Complete methods for report 7 and remove the 'name' from  enum Role

--- smdb-spring-project-api/src/main/java/gr/codelearn/smdb/api/controller/ReportController.java
+++ smdb-spring-project-api/src/main/java/gr/codelearn/smdb/api/controller/ReportController.java
@@ -2,7 +2,8 @@ package gr.codelearn.smdb.api.controller;
 
 import gr.codelearn.smdb.api.domain.Content;
 import gr.codelearn.smdb.api.domain.Genre;
-import gr.codelearn.smdb.api.helpers.YearGenresStats;
+import gr.codelearn.smdb.api.helpers.ContributorGenre;
+import gr.codelearn.smdb.api.helpers.YearGenresStat;
 import gr.codelearn.smdb.api.domain.Role;
 import gr.codelearn.smdb.api.service.ReportService;
 import gr.codelearn.smdb.api.transfer.ApiResponse;
@@ -10,6 +11,7 @@ import gr.codelearn.smdb.api.transfer.NoOfContentPerGenreDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -73,9 +75,18 @@ public class ReportController {
 	}
 
 	@GetMapping(path = "years")
-	public ResponseEntity<ApiResponse<List<YearGenresStats>>> getNoOfContentPerYearPerGenre() {
-		return ResponseEntity.ok(ApiResponse.<List<YearGenresStats>>builder()
+	public ResponseEntity<ApiResponse<List<YearGenresStat>>> getNoOfContentPerYearPerGenre() {
+		return ResponseEntity.ok(ApiResponse.<List<YearGenresStat>>builder()
 										 .data(reportService.getNoOfContentPerYearPerGenre())
 										 .build());
 	}
+
+	@GetMapping(path = "person/{pId}/contents/genre",
+				headers = "action=getAllContentOfContributorByIdPerGenres",
+				produces ="application/vnd.app-v1+json")
+	public ResponseEntity<ApiResponse<List<ContributorGenre>>> getAllContentOfContributorByIdPerGenres(@PathVariable(value = "pId") Long personId) {
+		return ResponseEntity.ok(ApiResponse.<List<ContributorGenre>>builder()
+										 .data(reportService.getAllContentOfContributorByIdPerGenres(personId)).build());
+	}
+
 }
