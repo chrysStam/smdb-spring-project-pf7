d2f3517abc23477a3300f535c588302194862a9c
Theodoros Mystiloglou <thodorismystiloglou@gmail.com>
2022-01-12 15:07:44 +0200

    Added findTopXByBoxOffice for films and findByCompletion for tvshows. Added more exception handlers.

--- smdb-spring-project-api/src/main/java/gr/codelearn/smdb/api/service/FilmService.java
+++ smdb-spring-project-api/src/main/java/gr/codelearn/smdb/api/service/FilmService.java
@@ -2,5 +2,9 @@ package gr.codelearn.smdb.api.service;
 
 import gr.codelearn.smdb.api.domain.Film;
 
+import java.util.List;
+
 public interface FilmService extends ContentService<Film> {
+
+	List<Film> findTopXOrderedByBoxOffice(Integer num);
 }
